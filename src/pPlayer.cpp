d84a76c8d02baac3a3b5cd827eff2e637d62b1f9
prasadshingne <prasadshingne@gmail.com>
2022-01-01 18:39:14 -0800

    modified pPlayer files

---
+++ src/pPlayer.cpp
@@ -0,0 +1,17 @@
+#include "pPlayer.h"
+
+pPlayer::pPlayer() {}
+pPlayer::~pPlayer() {}
+
+pPlayer::pPlayer(int x, int y, int width, int height, int red, int grn, int blu, int a, int d) :
+pMovers(x,y,width,height,red,grn,blu,a), _dir(d) {}
+
+int pPlayer::getDirection() const {
+    return _dir;
+    }
+void pPlayer::resetDirection() {
+    _dir = 0;
+    }
+void pPlayer::updateDirection(const int d) {
+    _dir += d;
+    }
--- src/pPlayer.h
+++ src/pPlayer.h
@@ -7,6 +7,14 @@ class pPlayer : public pMovers {
     public:
      pPlayer();
      ~pPlayer();
+     pPlayer(int x, int y, int width, int height, int red, int grn, int blu, int a, int d);
+
+     int getDirection() const;
+     void resetDirection();
+     void updateDirection(const int d);
+
+    private:
+     int _dir; // direction - up or down 
 };
 
 
